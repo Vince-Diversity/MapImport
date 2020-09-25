4bbe655dd8260b5b161fd6ff0932fe29afa9ae5c
Vince Diversity <vince.diversity@gmail.com>
2020-09-26 00:52:59 +0200

    importer has anim

---
+++ Importer.py
@@ -0,0 +1,73 @@
+from extract_prom_data import read_map_list
+import json
+import shutil
+from pathlib import Path
+
+# some sheets have been cut in two pieces so there is new indexing
+tileset_translate = {0:0, 1:1, 2:2, 3:3, 4:[4,5],
+	5:[6,7], 6:8, 7:[9,10], 8:[11,12], 9:13, 10:14,
+	11:15, 12:16, 13:17}
+# where each old sheet was cut, in terms of tile indexing
+cut_lengths = {"4": 4270, "5": 3920, "7": 5894, "8": 4774}
+
+TILE_KEYS = ["Ground", "Mask", "Mask2", "Mask3", "Fringe", "Fringe2",
+	"Fringe3"]
+ANIM_KEYS = ["GroundAnim", "MaskAnim", "Mask2Anim", "Mask3Anim",
+	"FringeAnim", "Fringe2Anim", "Fringe3Anim"]
+TILE_SET_KEYS = ["GroundTileset", "MaskTileset", "Mask2Tileset",
+	"Mask3Tileset", "FringeTileset", "Fringe2Tileset", "Fringe3Tileset"]
+ANIM_SET_KEYS = ["GroundAnimTileset", "MaskAnimTileset", "Mask2AnimTileset",
+	"Mask3AnimTileset", "FringeAnimTileset", "Fringe2AnimTileset",
+	"Fringe3AnimTileset"]
+
+# May give an offset to tile_index depending on which part it belongs to
+def index_offset(tileset_index, tile_index):
+	tileset_cutoff = cut_lengths[str(tileset_index)]
+	if int(tile_index/tileset_cutoff) >= 1:
+		part_index = 1
+		tile_index = tile_index - tileset_cutoff
+	else: part_index = 0
+	return tile_index, part_index
+
+def get_tile_sheet_indices(tile, tileset_index, tile_index):
+    sheet_index = tileset_translate[tileset_index]
+    if isinstance(sheet_index,(list)):
+        tile_index, part = index_offset(tileset_index, tile_index)
+        sheet_index = sheet_index[part]
+    return tile_index, sheet_index
+
+def copy_tile(tile, anim_sheet, anim_key):
+    anim_index = int(tile[anim_key])
+    anim_index, anim_sheet = get_tile_sheet_indices(tile, anim_sheet, anim_index)
+    anim_path = Path('SheetTiles/'+str(anim_sheet)+'/'+str(anim_index+1)+'.png')
+    target_path = Path('AnimTiles/'+str(anim_sheet)+'/'+str(anim_index)+'.png')
+    shutil.copy(anim_path, target_path)
+
+def copy_tiles(tiles):
+    for tile in tiles:
+        if tile['MapID'] == 's1622':
+            for key_index, anim_key in enumerate(ANIM_KEYS):
+                if tile[anim_key] != "0":
+                    anim_sheet = int(tile[ANIM_SET_KEYS[key_index]])
+                    copy_tile(tile, anim_sheet, anim_key)
+                    tile_sheet = int(tile[TILE_SET_KEYS[key_index]])
+                    copy_tile(tile, tile_sheet, TILE_KEYS[key_index])
+
+def extract_tiles(map):
+    with open('prom_data.json', 'r') as o2:
+        tiles = json.load(o2)
+        copy_tiles(tiles)
+
+def extract_animation_tiles(map_list):
+    print(map_list)
+    print('Copying tiles ...')
+    for map in map_list:
+        extract_tiles(map)
+    print('Copying complete!')
+
+def importer():
+    map_list = read_map_list()
+    extract_animation_tiles(map_list)
+
+if __name__ == "__main__":
+    importer()
---
+++ extract_prom_data.py
@@ -0,0 +1,56 @@
+import json
+
+line_format =  '{"MapID": "", "X": "", "Y": "", "Ground": "", \
+    "GroundAnim": "", "Mask": "", "MaskAnim": "", "Mask2": "", \
+    "Mask2Anim": "", "Fringe": "", "FringeAnim": "", "Fringe2": "", \
+    "Fringe2Anim": "", "Mask3": "", "Mask3Anim": "", "Fringe3": "", \
+    "Fringe3Anim": "", "Type": "", "Data1": "", "Data2": "", \
+    "Data3": "", "String1": "", "String2": "", "String3": "", \
+    "Light": "", "GroundTileset": "", "GroundAnimTileset": "", \
+    "MaskTileset": "", "MaskAnimTileset": "", "Mask2Tileset": "", \
+    "Mask2AnimTileset": "", "FringeTileset": "", "FringeAnimTileset": \
+    "", "Fringe2Tileset": "", "Fringe2AnimTileset": "", \
+    "Mask3Tileset": "", "Mask3AnimTileset": "", \
+    "Fringe3Tileset": "", "Fringe3AnimTileset": ""}'
+
+def read_map_list():
+    map_list = []
+    with open('prom_maps.txt', 'r') as oo:
+        maps = json.load(oo)
+#        for line in oo:
+        for name, map in maps.items():
+            tile_list = []
+            for map_row in map:
+                for map_piece in map_row:
+                    map_list.append(map_piece)
+#            map_list.append(line.replace("\n", ""))
+    return map_list
+
+def extract():
+    prom_path = 'prom_data.json'
+    with open('good_map_dicts.json','r') as messy_file:
+        write_all(messy_file, prom_path)
+
+# only when last line is part of map list does this work
+def write_all(messy_file, target_path):
+    open(target_path,'w').close()
+    with open(target_path,'a+') as target:
+        target.write('{\n')
+        extract_prom(messy_file, target)
+        target.write(line_format)   # so the last comma gives something
+        target.write('\n]')
+
+def extract_prom(messy_file, target):
+    map_list = read_map_list()
+    print(map_list)
+    for line in messy_file:
+        write_next(map_list, target, line)
+
+def write_next(map_list, target, line):
+    tile = json.loads(line)
+    if tile['MapID'] in map_list:
+        json.dump(tile, target)
+        target.write(',\n')
+
+if __name__ == "__main__":
+    extract()
